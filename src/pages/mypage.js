10a5c4553836befcc2d6202bd66a9c0af635391d
Liv Hartoft Borre <liv.borre@gmail.com>
2020-10-10 03:08:49 +0200

    retry

---
+++ src/pages/mypage.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Header from "../components/header";
+import "./mystyle.scss";
+
+export const MyPage = () => (
+  <div>
+    <section class="hero is-fullheight is-dark is-bold">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <p class="title">MANNA</p>
+          <p class="subtitle">stay tuned...</p>
+        </div>
+      </div>
+    </section>
+    {/* <section class="hero is-fullheight ">
+      <div class="tile is-ancestor">
+        <div class="tile is-vertical is-8">
+          <div class="tile">
+            <div class="tile is-parent is-vertical">
+              <article class="tile is-child box">Here</article>
+              <article class="tile is-child box">Is</article>
+            </div>
+            <div class="tile is-parent">
+              <article class="tile is-child box">Some</article>
+            </div>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child box">Content</article>
+          </div>
+        </div>
+        <div class="tile is-parent">
+          <article class="tile is-child box">!</article>
+        </div>
+      </div>
+    </section> */}
+  </div>
+);
+
+export default MyPage;
